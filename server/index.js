60553281ebed1562119806f38595062146a508ed
Swapneswarnayak <1705835@kiit.ac.in>
2023-01-09 16:05:53 +0530

    initial commit

--- server/index.js
+++ server/index.js
@@ -26,3 +26,16 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/assets", express.static(path.join(__dirname, "public/assets")));
+
+//file-storage multer
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/assets");
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
