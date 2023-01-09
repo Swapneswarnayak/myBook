4da54ce7beed282e575b0c5e43d6b7d982f1def5
Swapneswarnayak <1705835@kiit.ac.in>
2023-01-09 16:00:13 +0530

    middleware added

--- server/index.js
+++ server/index.js
@@ -2,9 +2,27 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import multer from "multer";
 import helmet from "helmet";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+
+// configurations
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config(); //to access enviroment file
+const app = express();
+
+// middlewares
+
+app.use(express.json()); //accept req body as json
+app.use(helmet()); //req safely
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common")); //logger
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
