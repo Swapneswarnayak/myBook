66eeecadfa2c2ddb3f18dc3ff131df8bd67fa098
Swapneswarnayak <1705835@kiit.ac.in>
2023-01-22 19:44:41 +0530

    frontend in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,21 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Homepage/Home";
+import Login from "./Pages/Loginpage/Login";
+import Navbar from "./Pages/Navbar/Navbar";
+import Profile from "./Pages/Profilepage/Profile";
+
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/profile/:userId" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
